10048257768c7ab2c37282ae6053260aebfd5aa6
Anson <ansonox@gmail.com>
2016-09-25 02:03:09 +0100

    Add http get service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Slot, Day } from './shared/slot';
+import {Http, Response} from '@angular/http';
+
+// Add the RxJS Observable operators we need in this app.
+import './rxjs-operators';
+
+//Mock Data
 import { TIMETABLE } from './mockdata/timetable';
 
 @Component({
@@ -8,24 +13,43 @@ import { TIMETABLE } from './mockdata/timetable';
   templateUrl: './app.component.html'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  btnSelectedDay = true;
 
   title = 'Lancaster University Societies Timetable';
-  timetable = TIMETABLE;
-  firstload = true;
+  // timetable = TIMETABLE;
+  timetable = [];
+  
   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   selectedDays = this.days;
 
   msg = 'No activities today :(';
 
+  constructor(private http: Http) { }
+
   onSelect(day){
-    this.firstload = false;
+    this.btnSelectedDay = false;
     if(day == 'all'){
-      this.firstload = true;
+      this.btnSelectedDay = true;
       this.selectedDays = this.days;
       return;
     }
     this.selectedDays = [day];
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData(){
+    this.http.get('http://v1.api.societies.online/timetable')
+    .map((res:Response) => res.json())
+      .subscribe(
+        data => this.timetable = data,
+        err => console.error(err),
+        () => console.log('done')
+      );
   }
 
 }
